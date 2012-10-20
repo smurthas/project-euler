5c6389d1663b58c8fca77cd773801430eac124ec
Simon Murtha-Smith <simon@murtha-smith.com>
2012-10-19 18:44:50 -0700

    4

---
+++ 004/index.js
@@ -0,0 +1,20 @@
+function isPalindrome(num) {
+  var str = num.toString();
+  for (var i=0; i < str.length/2; i++) {
+    if (str.charAt(i) !== str.charAt(str.length - 1 - i)) return false;
+  }
+  return true;
+}
+
+//var MAX = 99;
+var MAX = 999;
+
+var largestPalindrome = 0;
+for (var i = MAX; i > 0; i--) {
+  for (var j = MAX; j > 0; j--) {
+    var k = i*j;
+    if (k > largestPalindrome && isPalindrome(k)) largestPalindrome = k;
+  }
+}
+
+console.log(largestPalindrome);
